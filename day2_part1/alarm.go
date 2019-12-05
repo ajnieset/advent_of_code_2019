82791f672be6f82825f0c09afacc5707ddeaee32
Austin Nieset <ajnieset@gmail.com>
2019-12-04 23:19:27 -0500

    Renamed variables

--- day2_part1/alarm.go
+++ day2_part1/alarm.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	const delim = ','
-	var vals = make([]int64, 173)
+	var opcodes = make([]int64, 173)
 
 	file, err := os.Open("github.com/ajnieset/advent_of_code/day2_part1/input.txt")
 	if err != nil {
@@ -29,21 +29,21 @@ func main() {
 	fmt.Printf("Size of r: %d\n", r.Size())
 
 	for i := 0; i < 173; i++ {
-		opcodes, err := r.ReadString(delim)
-		if opcodes[:len(opcodes)-1] != ""{
-			opcodes = opcodes[:len(opcodes)-1]
+		opcode, err := r.ReadString(delim)
+		if opcode[:len(opcode)-1] != ""{
+			opcode = opcode[:len(opcode)-1]
 		}
 		if err != nil {
 			if err == io.EOF {
 				fmt.Printf("Value of I: %d\n", i)
-				fmt.Printf("Value of last number: %q\n", opcodes)
+				fmt.Printf("Value of last number: %q\n", opcode)
 				break
 			}
 			fmt.Println(err)
 			return
 		}
 
-		vals[i], err = strconv.ParseInt(opcodes,10,0)
+		opcodes[i], err = strconv.ParseInt(opcode,10,0)
 		if err != nil {
 			fmt.Println(err)
 			return
