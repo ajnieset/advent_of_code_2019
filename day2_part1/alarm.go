83c1d4d3fb532d474d338c779e99c6a8a4ca6305
Austin Nieset <ajnieset@gmail.com>
2019-12-05 22:50:13 -0500

    Began implementing opcode reader

--- day2_part1/alarm.go
+++ day2_part1/alarm.go
@@ -26,8 +26,6 @@ func main() {
 	
 	r := bufio.NewReader(file)
 
-	fmt.Printf("Size of r: %d\n", r.Size())
-
 	for {
 		input, err := r.ReadString(delim)
 		if input[:len(input)-1] != ""{
@@ -35,7 +33,12 @@ func main() {
 		}
 		if err != nil {
 			if err == io.EOF {
-				fmt.Printf("Value of last number: %q\n", input)
+				opcode, err := strconv.Atoi(input)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				opcodes = append(opcodes, opcode)
 				break
 			}
 			fmt.Println(err)
@@ -43,11 +46,33 @@ func main() {
 		}
 
 		opcode, err := strconv.Atoi(input)
-		opcodes = append(opcodes, opcode)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		opcodes = append(opcodes, opcode)
+	}
+	fmt.Printf("Last Number: %d\n", opcodes[len(opcodes)-1])
+	fmt.Printf("Second to Last Number: %d\n", opcodes[len(opcodes)-2])
+	fmt.Printf("Size of slice: %d\n", len(opcodes))
+
+}
+
+func opcode_program(opcodes []int) int {
+
+	pos := 0
+	for {
+		if opcodes[pos] == 1 {
+			val1 := opcodes[opcodes[pos+1]]
+			val2 := opcodes[opcodes[pos+2]]
+
+			result := val1 + val2
+
+			opcodes[opcodes[pos+3]] = result
+		} else if opcodes[pos] == 99 {
+			break
+		}
 	}
 
+	return opcodes[0]
 }
